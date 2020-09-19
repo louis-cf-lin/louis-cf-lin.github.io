a749fc7839c774b98c9feea229b6447bda824601
Louis <clin750@aucklanduni.ac.nz>
2020-09-20 11:23:47 +1200

    Full page attempt

---
+++ scripts/javascript.js
@@ -0,0 +1,10 @@
+new fullpage('#fullpage', {
+  //options here
+  licenseKey: '1A848552-FFD34058-A6D0F08A-F33DC2F0',
+  autoScrolling: true,
+  scrollHorizontally: false,
+  navigation: true,
+  navigationPosition: 'left',
+  navigationTooltips: ['Home']
+})
+story.size = true
