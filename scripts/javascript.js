6aae7df08c5bcbd9a5b6f0909a94f5920571fd45
Louis <clin750@aucklanduni.ac.nz>
2020-09-20 13:31:20 +1200

    Functioning fullPage.js

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -3,8 +3,15 @@ new fullpage('#fullpage', {
   licenseKey: '1A848552-FFD34058-A6D0F08A-F33DC2F0',
   autoScrolling: true,
   scrollHorizontally: false,
-  navigation: true,
-  navigationPosition: 'left',
-  navigationTooltips: ['Home']
+  menu: '#menu',
+  anchors: ['section-1', 'section-2', 'section-3', 'section-4', 'section-5', 'section-6'],
+  lockAnchors: true
 })
-story.size = true
+// story.size = true
+
+document.querySelectorAll('.menu-item').forEach((el, i) => {
+  el.addEventListener('click', () => {
+    console.log('hi')
+    fullpage_api.moveTo(i + 1)
+  })
+})
